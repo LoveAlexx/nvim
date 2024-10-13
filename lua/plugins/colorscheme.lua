9130a1ed320c72235222c83965f9f55479d1cc2f
Alexa <alexxsandar52@gmail.com>
2024-10-13 02:41:32 +0200

    first commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  -- add gruvbox
+  { "projekt0n/github-nvim-theme" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_dark_high_contrast",
+    },
+  },
+}
+--return {
+-- add gruvbox
+--  { "ellisonleao/gruvbox.nvim" },
+--
+-- Configure LazyVim to load gruvbox
+-- {
+--   "LazyVim/LazyVim",
+--   opts = {
+--  colorscheme = "gruvbox",
+--  },
+-- },
+--}
